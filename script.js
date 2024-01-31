7827f1af961102a8c1b95643c1b43f6970e4450a
christopher-kenway <maero1eguwe@gmail.com>
2024-01-31 08:28:46 +0100

    new shii

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
-$(document).ready(function() {
+/*$(document).ready(function() {
 
     $nav = $('.navbar__nav');
     $toggleCollapse = $('.toggle-collapse');
 
     // click event on toggle menu
     $toggleCollapse.click(function(){
-        $nav.toggleClass('navbar__links');
+        $nav.toggleClass('collapse');
     })
-});
+});*/
+
+function $toggleCollapse() {
+    var x = document.getElementById("ToggleOpen");
+    if (x.className === "navbar__nav") {
+        x.className += " collapse";
+    } else {
+        x.className = "navbar__nav";
+    }
+  }
